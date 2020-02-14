d316295fcb710a0eb41ae304cb133e3fa39af044
dr_blink <andrea.manna7@gmail.com>
2020-02-14 23:15:14 +0100

    css removed, added tests path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import './App.css'
 
 export function App() {
   return (
-    <>
-    <header className="App-header"></header>
-    <div className="App"></div>
-    <footer>test</footer>
-    </>
+    <Router>
+    <div>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/test">
+          <Test />
+        </Route>
+        <Route path="/prova">
+          <Prova />
+        </Route>
+        <Route path="/">
+          <Redirect to={"/Home"} />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
   )
+}
+
+function Home(){
+  return <>
+    Home
+  </>
+}
+
+function Test(){
+  return <>
+    Test
+  </>
+}
+
+function Prova(){
+  return <>
+    Prova
+  </>
 }
