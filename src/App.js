44aabed8a6ca3d8520e2fcd94b74db017cf2e2e2
dr_blink <andrea.manna7@gmail.com>
2020-02-15 14:23:02 +0100

    NaN removed

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ import './App.css'
 export function App() {
   return (
     <Router>
-    <div>
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/test">
-          <Test />
-        </Route>
-        <Route path="/prova">
-          <Prova />
-        </Route>
-        <Route path="/">
-          <Redirect to={"/Home"} />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+      <div>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/prova">
+            <Prova />
+          </Route>
+          <Route path="/">
+            <Redirect to={"/Home"} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
 export const useMousePosition = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 ,e: '',posX:0,posY:0});
+  const [position, setPosition] = useState({ x: 0, y: 0, e: '', posX: 0, posY: 0 });
   useEffect(() => {
     const setFromEvent = e => {
-    setPosition({ x: e.clientX, y: e.clientY ,e: e, posX: e.target.x, posY: e.target.y});
+      setPosition({ x: e.clientX, y: e.clientY, e: e, posX: e.target.x, posY: e.target.y });
     }
     window.addEventListener("mousemove", setFromEvent);
     return () => {
@@ -40,25 +40,25 @@ export const useMousePosition = () => {
   return position;
 };
 
-function clickHandler(x,y){
-  console.log(x,y)
+function clickHandler(x, y) {
+  console.log(x, y)
   let clickPos = ""
   /**
    *  y: 141 - 187
    *  x: 545 - 605
-   * */ 
-  if((x>=545 && x <= 605) && (y>= 141 && y <= 187))
-    clickPos="isolaBoschetto"
+   * */
+  if ((x >= 545 && x <= 605) && (y >= 141 && y <= 187))
+    clickPos = "isolaBoschetto"
   /**
    * x: 474 - 515
    * y: 118 - 148
    */
-  else if((x>=474 && x<= 515) && (y>= 118 && y<=148)){
-    clickPos="isolaDelFaro"
+  else if ((x >= 474 && x <= 515) && (y >= 118 && y <= 148)) {
+    clickPos = "isolaDelFaro"
   }
   else
-    clickPos="not registered"
-  console.log("hai clickato: ",clickPos)
+    clickPos = "not registered"
+  console.log("hai clickato: ", clickPos)
 
 }
 
@@ -66,26 +66,37 @@ function Home() {
   let position = useMousePosition();
   return (
     <div className="App">
-    <div className="header">head</div>
-  <div className="container">
-    <div className="test-map" onClick={() => clickHandler(position.x-position.posX, position.y-position.posY)}>
-      <img src="/test.png" alt = "test-map"></img>
+      <div className="header">head</div>
+      <div className="container">
+        <div className="test-map" onClick={() => clickHandler(position.x - position.posX, position.y - position.posY)}>
+          <img src="/test.png" alt="test-map"></img>
+        </div>
+      </div>
+      <div className="footer"><div>
+        {isNaN(position.x - position.posX) ?
+          ""
+          :
+          position.x - position.posX + " : "
+        }
+        {
+          isNaN(position.y - position.posY) ?
+            ""
+            :
+            position.y - position.posY}
+        <br />
+      </div>
+      </div>
     </div>
-  </div>
-  <div className="footer"><div>
-      {position.x-position.posX}:{position.y-position.posY}<br />
-    </div></div>
-  </div>
   )
 }
 
-function Test(){
+function Test() {
   return <>
     Test
   </>
 }
 
-function Prova(){
+function Prova() {
   return <>
     Prova
   </>
