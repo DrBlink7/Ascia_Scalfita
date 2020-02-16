fbe83cdcf07048519a6fb8ad4d21c112d268ba03
dr_blink <andrea.manna7@gmail.com>
2020-02-16 17:51:01 +0100

    generalizzato clickHandler

--- src/LorynnKingdom.js
+++ src/LorynnKingdom.js
@@ -1,27 +1,20 @@
 import { View } from './service'
+import { getLorynnKingdomPosition } from './mock'
 
-export function LorynnKingdom(){
-  return View("App","/test.png",lorynnKingdomClickHandler)
+export function LorynnKingdom() {
+  return View("App", "/test.png", lorynnKingdomClickHandler)
 }
 
-
 const lorynnKingdomClickHandler = (x, y) => {
-  let clickPos = ""
-  /**
-   * x: 696 - 775
-   * y: 153 - 210
-   * */
-  if ((x >= 696 && x <= 775) && (y >= 153 && y <= 210)) {
-    clickPos = "Isola_del_Boschetto"
-  }
-  /**
-   * x: 604 - 661
-   * y: 123 - 161
-   */
-  else if ((x >= 604 && x <= 661) && (y >= 123 && y <= 161)) {
-    clickPos = "Isola_del_Faro"
-  }
-  else
-    clickPos = "not registered"
-  return "/" + clickPos
+  let location = getLorynnKingdomPosition()
+  return clickHandler(location, x, y)
+}
+
+function clickHandler(location, x, y) {
+  let newLocation = "not registered"
+  location.forEach(loc => {
+    if ((x >= loc.xMin && x <= loc.xMax) && (y >= loc.yMin && y <= loc.yMax))
+      newLocation = "/" + loc.label
+  })
+  return newLocation
 }
---
+++ src/mock.js
@@ -0,0 +1,37 @@
+export function getLorynnKingdomPosition() {
+  return [
+    {
+      label: "Isola_del_Boschetto",
+      xMin: 696,
+      xMax: 775,
+      yMin: 153,
+      yMax: 210
+    },
+    {
+      label: "Isola_del_Faro",
+      xMin: 604,
+      xMax: 661,
+      yMin: 123,
+      yMax: 161
+    }
+  ]
+}
+
+export function getIsolaBoschettoPosition() {
+  return [
+    {
+      label: "Isola_del_Boschetto",
+      xMin: 696,
+      xMax: 775,
+      yMin: 153,
+      yMax: 210
+    },
+    {
+      label: "Isola_del_Faro",
+      xMin: 604,
+      xMax: 661,
+      yMin: 123,
+      yMax: 161
+    }
+  ]
+}
